2537bb661a02d3706e69410be2131104c2c9b987
zamanrabeen671 <manbeen671@gmail.com>
2021-08-10 23:27:25 +0600

    bug fixed

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import NewLeds from "../src/components/NewLeds";
 import Sales from "../src/components/Sales";
 import Orders from "../src/components/Orders";
--- src/components/Newleds.js
+++ src/components/Newleds.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { AiOutlineUserAdd } from 'react-icons/ai';
